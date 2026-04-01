e7e57fe84b149b13d66f43bfc311803bfd14b564
muonroi <muonroi@outlook.com>
2026-04-01 19:27:35 +0700

    test(76-03): add SiteGrpcDispatchHelperTests and SiteGrpcClientFactoryTests

---
+++ tests/Muonroi.Tenancy.SiteProfile.Grpc.Tests/SiteGrpcClientFactoryTests.cs
@@ -0,0 +1,131 @@
+using FluentAssertions;
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Xunit;
+
+namespace Muonroi.Tenancy.SiteProfile.Grpc.Tests;
+
+public sealed class SiteGrpcClientFactoryTests
+{
+    private interface IFakeFacade { }
+    private class FakeFacadeImpl : IFakeFacade { }
+
+    private abstract class FakeClient : ClientBase
+    {
+        protected FakeClient() : base() { }
+        protected FakeClient(CallInvoker callInvoker) : base(callInvoker) { }
+    }
+
+    private class ConcreteFakeClient : FakeClient
+    {
+        public ConcreteFakeClient() : base() { }
+        public ConcreteFakeClient(CallInvoker callInvoker) : base(callInvoker) { }
+    }
+
+    private readonly ISiteProfileResolver _resolver;
+    private readonly SiteGrpcClientRegistry _registry;
+    private readonly GrpcClientFactoryAccessor _accessor;
+    private readonly IServiceCollection _services;
+    private SiteGrpcClientFactory _factory = null!;
+
+    public SiteGrpcClientFactoryTests()
+    {
+        _resolver = Substitute.For<ISiteProfileResolver>();
+        _registry = new SiteGrpcClientRegistry();
+        _accessor = new GrpcClientFactoryAccessor();
+        _services = new ServiceCollection();
+    }
+
+    private void BuildFactory()
+    {
+        _factory = new SiteGrpcClientFactory(_resolver, _registry, _accessor, _services.BuildServiceProvider());
+    }
+
+    [Fact]
+    public void CreateForCurrentSite_ResolvesCorrectDescriptor_ButCreateClientReturnsNull_ThrowsInvalidOperationException()
+    {
+        _resolver.Current.SiteId.Returns("TCI");
+        var descriptor = new SiteGrpcClientDescriptor("TCI", "test-svc", typeof(ConcreteFakeClient));
+        _registry.Add(descriptor);
+        BuildFactory();
+
+        var act = () => _factory.CreateForCurrentSite<ConcreteFakeClient>("test-svc");
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*No gRPC client cached*");
+    }
+
+    [Fact]
+    public void CreateForCurrentSite_FallsBackToDefault_ButCreateClientReturnsNull_ThrowsInvalidOperationException()
+    {
+        _resolver.Current.SiteId.Returns("UNKNOWN");
+        var descriptor = new SiteGrpcClientDescriptor("default", "test-svc", typeof(ConcreteFakeClient));
+        _registry.Add(descriptor);
+        BuildFactory();
+
+        var act = () => _factory.CreateForCurrentSite<ConcreteFakeClient>("test-svc");
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*No gRPC client cached*");
+    }
+
+    [Fact]
+    public void CreateForCurrentSite_UnknownSite_NoDefault_ThrowsInvalidOperationException()
+    {
+        _resolver.Current.SiteId.Returns("TCI");
+        BuildFactory();
+
+        var act = () => _factory.CreateForCurrentSite<ConcreteFakeClient>("test-svc");
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*No gRPC client registered for site 'TCI'*");
+    }
+
+    [Fact]
+    public void CreateForCurrentSite_NullServiceName_ThrowsArgumentException()
+    {
+        BuildFactory();
+        var act = () => _factory.CreateForCurrentSite<ConcreteFakeClient>("");
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void CreateFacadeForCurrentSite_ResolvesKeyedFacade()
+    {
+        _resolver.Current.SiteId.Returns("TCI");
+        var expectedFacade = new FakeFacadeImpl();
+        _services.AddKeyedSingleton<IFakeFacade>("facade:test-svc:TCI", expectedFacade);
+        BuildFactory();
+
+        var result = _factory.CreateFacadeForCurrentSite<IFakeFacade>("test-svc");
+
+        result.Should().BeSameAs(expectedFacade);
+    }
+
+    [Fact]
+    public void CreateFacadeForCurrentSite_FallsBackToDefault()
+    {
+        _resolver.Current.SiteId.Returns("UNKNOWN");
+        var expectedFacade = new FakeFacadeImpl();
+        _services.AddKeyedSingleton<IFakeFacade>("facade:test-svc:default", expectedFacade);
+        BuildFactory();
+
+        var result = _factory.CreateFacadeForCurrentSite<IFakeFacade>("test-svc");
+
+        result.Should().BeSameAs(expectedFacade);
+    }
+
+    [Fact]
+    public void CreateFacadeForCurrentSite_NoFacade_ThrowsInvalidOperationException()
+    {
+        _resolver.Current.SiteId.Returns("TCI");
+        BuildFactory();
+
+        var act = () => _factory.CreateFacadeForCurrentSite<IFakeFacade>("test-svc");
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*No gRPC facade registered for site 'TCI'*");
+    }
+}
---
+++ tests/Muonroi.Tenancy.SiteProfile.Grpc.Tests/SiteGrpcDispatchHelperTests.cs
@@ -0,0 +1,108 @@
+using FluentAssertions;
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Muonroi.Logging.Abstractions;
+using NSubstitute;
+using Xunit;
+
+namespace Muonroi.Tenancy.SiteProfile.Grpc.Tests;
+
+public sealed class SiteGrpcDispatchHelperTests
+{
+    public abstract class FakeServiceBase
+    {
+        public abstract Task<string> DoWork(ServerCallContext ctx);
+    }
+
+    public class FakeTciService : FakeServiceBase
+    {
+        public override Task<string> DoWork(ServerCallContext ctx) => Task.FromResult("TCI-WORK");
+    }
+
+    public class FakeDefaultService : FakeServiceBase
+    {
+        public override Task<string> DoWork(ServerCallContext ctx) => Task.FromResult("DEFAULT-WORK");
+    }
+
+    private readonly IServiceProvider _serviceProvider;
+    private readonly SiteCodeHolder _siteCodeHolder;
+    private readonly SiteGrpcDispatchHelper<FakeServiceBase> _helper;
+
+    public SiteGrpcDispatchHelperTests()
+    {
+        var services = new ServiceCollection();
+        services.AddKeyedScoped<FakeServiceBase, FakeTciService>("TCI");
+        services.AddKeyedScoped<FakeServiceBase, FakeDefaultService>("default");
+        _serviceProvider = services.BuildServiceProvider();
+
+        _siteCodeHolder = new SiteCodeHolder();
+        var log = Substitute.For<IMLog<SiteGrpcDispatchHelper<FakeServiceBase>>>();
+        _helper = new SiteGrpcDispatchHelper<FakeServiceBase>(_serviceProvider, _siteCodeHolder, log);
+    }
+
+    [Fact]
+    public async Task ExactSiteMatch_DispatchesToCorrectHandler()
+    {
+        _siteCodeHolder.SiteCode = "TCI";
+        var context = Substitute.For<ServerCallContext>();
+
+        var result = await _helper.DispatchAsync(context, (h, ctx) => h.DoWork(ctx));
+
+        result.Should().Be("TCI-WORK");
+    }
+
+    [Fact]
+    public async Task FallbackToDefault_WhenSiteNotRegistered()
+    {
+        _siteCodeHolder.SiteCode = "UNKNOWN";
+        var context = Substitute.For<ServerCallContext>();
+
+        var result = await _helper.DispatchAsync(context, (h, ctx) => h.DoWork(ctx));
+
+        result.Should().Be("DEFAULT-WORK");
+    }
+
+    [Fact]
+    public async Task NoHandler_ThrowsRpcException()
+    {
+        var services = new ServiceCollection();
+        // No registrations
+        var sp = services.BuildServiceProvider();
+        var helper = new SiteGrpcDispatchHelper<FakeServiceBase>(sp, _siteCodeHolder, Substitute.For<IMLog<SiteGrpcDispatchHelper<FakeServiceBase>>>());
+        
+        _siteCodeHolder.SiteCode = "TCI";
+        var context = Substitute.For<ServerCallContext>();
+
+        var act = () => helper.DispatchAsync(context, (h, ctx) => h.DoWork(ctx));
+
+        await act.Should().ThrowAsync<RpcException>()
+            .Where(e => e.StatusCode == StatusCode.Internal);
+    }
+
+    [Fact]
+    public async Task NullSiteCode_DefaultsToDefaultKey()
+    {
+        _siteCodeHolder.SiteCode = null;
+        var context = Substitute.For<ServerCallContext>();
+
+        var result = await _helper.DispatchAsync(context, (h, ctx) => h.DoWork(ctx));
+
+        result.Should().Be("DEFAULT-WORK");
+    }
+
+    [Fact]
+    public async Task DispatchAsync_PassesContextToHandler()
+    {
+        _siteCodeHolder.SiteCode = "TCI";
+        var context = Substitute.For<ServerCallContext>();
+        
+        var receivedContext = (ServerCallContext)null!;
+        await _helper.DispatchAsync(context, (h, ctx) =>
+        {
+            receivedContext = ctx;
+            return h.DoWork(ctx);
+        });
+
+        receivedContext.Should().BeSameAs(context);
+    }
+}
